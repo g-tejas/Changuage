1a179c646bf52dacac17304b580e23747591c1e5
Archit <architg.sch@gmail.com>
2023-01-15 01:22:16 +0800

    Updated ant design app.tsx

--- src/popup/app.tsx
+++ src/popup/app.tsx
@@ -1,6 +1,9 @@
 import { useColor } from '@/hooks/useColor';
 import { composeIssuePredicates } from 'fork-ts-checker-webpack-plugin/lib/issue/IssuePredicate';
 import { useState } from 'react';
+import { Button } from 'antd';
+
+
 
 
 const extensionVersion = chrome.runtime.getManifest().manifest_version;
@@ -28,19 +31,19 @@ export const App = () => {
   return (
     <div>
       Status: {transType}
-      <button value="convert %ORIGINALTEXT% to shakespearean" onClick={handleClick}>Shakespearean Speak</button>
-      <button value="'%ORIGINALTEXT%'. Translate it to gen z slang" onClick={handleClick}>Gen-Z Speak</button>
-      <button value="translate %ORIGINALTEXT% with many emojis" onClick={handleClick}>Emoji Speak</button>
-      <button value="translate %ORIGINALTEXT% to legal contract speak" onClick={handleClick}>Legal Contract Speak</button>
-      <button value="write %ORIGINALTEXT% in UwU speak" onClick={handleClick}>UwU Speak</button>
-      <button value="write %ORIGINALTEXT% in pirate speak" onClick={handleClick}>Pirate Speak</button>
-      <button value="translate %ORIGINALTEXT% into cowboy speak" onClick={handleClick}>Cowboy Speak</button>
-      <button value="translate %ORIGINALTEXT% into valley girl speak" onClick={handleClick}>Valley girl Speak</button>
-      <button value="translate %ORIGINALTEXT% into a gangsta from da hood" onClick={handleClick}>Valley girl Speak</button>
-      <button value="'%ORIGINALTEXT%'. make it sound extremely offensive to the user" onClick={handleClick}>Offensive Speak</button>
-      <button value="%ORIGINALTEXT%. Make it extremely short." onClick={handleClick}>Compact Speak</button>
-      <button value="%ORIGINALTEXT%. Make it sound like someone who is extremely confused about everything" onClick={handleClick}>Confused Speak</button>
-      <button value="stop" onClick={handleClick}>STOP</button>
+      <Button type="primary" value="convert %ORIGINALTEXT% to shakespearean" onClick={handleClick}>Shakespearean Speak</Button>
+      <Button value="'%ORIGINALTEXT%'. Translate it to gen z slang" onClick={handleClick}>Gen-Z Speak</Button>
+      <Button value="translate %ORIGINALTEXT% with many emojis" onClick={handleClick}>Emoji Speak</Button>
+      <Button value="translate %ORIGINALTEXT% to legal contract speak" onClick={handleClick}>Legal Contract Speak</Button>
+      <Button value="write %ORIGINALTEXT% in UwU speak" onClick={handleClick}>UwU Speak</Button>
+      <Button value="write %ORIGINALTEXT% in pirate speak" onClick={handleClick}>Pirate Speak</Button>
+      <Button value="translate %ORIGINALTEXT% into cowboy speak" onClick={handleClick}>Cowboy Speak</Button>
+      <Button value="translate %ORIGINALTEXT% into valley girl speak" onClick={handleClick}>Valley girl Speak</Button>
+      <Button value="translate %ORIGINALTEXT% into a gangsta from da hood" onClick={handleClick}>Valley girl Speak</Button>
+      <Button value="'%ORIGINALTEXT%'. make it sound extremely offensive to the user" onClick={handleClick}>Offensive Speak</Button>
+      <Button value="%ORIGINALTEXT%. Make it extremely short." onClick={handleClick}>Compact Speak</Button>
+      <Button value="%ORIGINALTEXT%. Make it sound like someone who is extremely confused about everything" onClick={handleClick}>Confused Speak</Button>
+      <Button value="stop" onClick={handleClick}>STOP</Button>
     </div>
   );
 };
